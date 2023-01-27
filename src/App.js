34c7634d6ddc113d9d6be5ae49e9dc97c181f251
Gabriel52 <gabrielbrune52@gmail.com>
2023-01-27 00:38:29 -0300

    feat: add google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { GlobalStyled } from "./styles/global";
+import ReactGA from 'react-ga';
+
 
 import { Container } from "./components/Container/Container";
 
 import './i18n'
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=>{
+    ReactGA.initialize(process.env.REACT_APP_ID_METRIC_GA);
+  })
+
   return (
     <>
       <Container />
